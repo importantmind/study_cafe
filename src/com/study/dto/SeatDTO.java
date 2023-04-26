ae3246f4959be4507e353e8f8281d81beed59874
kimhaeduen <eun06151@naver.com>
2023-04-26 20:51:11 +0900

    잘못된 복사 삭제

--- src/com/study/dto/SeatDTO.java
+++ src/com/study/dto/SeatDTO.java
@@ -33,36 +33,4 @@ public class SeatDTO {
 		return "SeatDTO [num=" + num + ", set=" + set + "]";
 	}
 
-	private int num;
-	private boolean set = false;
-	
-	public SeatDTO() {}
-
-	public SeatDTO(int num, boolean set) {
-		this.num = num;
-		this.set = set;
-	}
-
-	public int getNum() {
-		return num;
-	}
-
-	public void setNum(int num) {
-		this.num = num;
-	}
-
-	public boolean isSet() {
-		return set;
-	}
-
-	public void setSet(boolean set) {
-		this.set = set;
-	}
-
-	@Override
-	public String toString() {
-		return "SeatDTO [num=" + num + ", set=" + set + "]";
-	}
-	
-
 }
